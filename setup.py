14b93b2e53b386a811c9bfccfbdd0459a810bcb7
Sam Ryan <samuelryan18@gmail.com>
2020-07-22 18:43:36 -0700

    update versioning (#2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hang_timer",
-    version="0.0",
+    version="0.0.0",
     author_email="samuelryan18@gmail.com",
     packages=["hang_timer"],
     python_requires=">=3.4",
